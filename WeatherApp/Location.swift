620764ffaf4ef647218ab4486653d015f800282c
alexanderdombroski <dom21013@byui.edu>
2024-05-25 10:23:04 -0600

    Locations can be saved and added to persistant data

--- WeatherApp/Location.swift
+++ WeatherApp/Location.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-final class Item {
+final class Location {
     var timestamp: Date
     
     init(timestamp: Date) {
---
+++ WeatherApp/LocationAdder.swift
@@ -0,0 +1,18 @@
+//
+//  LocationAdder.swift
+//  WeatherApp
+//
+//  Created by Alex Dombroski on 5/25/24.
+//
+
+import SwiftUI
+
+struct LocationAdder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationAdder()
+}
